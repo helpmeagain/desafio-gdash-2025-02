31bae3c19a443b085665adf9f25e86f8302a0d7d
helpmeagain <felipe.comarques1208@gmail.com>
2025-11-19 15:36:21 -0300

    feat: add nest api boilerplate

---
+++ packages/eslint-config/nest.js
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { globalIgnores } from "eslint/config";
+
+/**
+ * ESLint config para backend NestJS (flat config)
+ *
+ * @type {import("eslint").Linter.Config[]}
+ */
+export const nestConfig = [
+  globalIgnores(["eslint.config.mjs"]), 
+  js.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: "commonjs",
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "prettier/prettier": ["error", { endOfLine: "auto" }],
+    },
+  },
+];
